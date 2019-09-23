bffc51398dd893a1005b0eaddc4acd1db4798035
Job <j.vandermeiden@first8.nl>
2019-09-23 16:59:20 +0200

    updated server to include history

--- socket-server/src/app.js
+++ socket-server/src/app.js
@@ -3,12 +3,21 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 var game = {};
+var history = [];
+var currentPhase = 0;
 
 io.on('connection', socket => {
 
     socket.on('updateGame', incommingGame => {
         console.log("update game: ", incommingGame)
         game = incommingGame;
+        if (game.phase !== currentPhase) {
+            history.push(game);
+            currentPhase = game.phase;
+            console.log("Sending History.");
+            socket.emit('history', history);
+            socket.broadcast.emit('history', history)
+        }
         socket.broadcast.emit('game', incommingGame);
         socket.emit('game', incommingGame);
     });
@@ -16,12 +25,12 @@ io.on('connection', socket => {
     socket.on('resetGame', () => {
         console.log("Resetting game.")
         game = {};
+        history = [];
         socket.broadcast.emit('game', game);
         socket.emit('game', game);
     });
 
     io.emit('game', game);
-
     console.log(`Socket ${socket.id} has connected`);
 });
 
